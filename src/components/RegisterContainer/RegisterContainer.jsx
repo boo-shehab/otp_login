3d6e82add4264bca1806659053593206642d2fd5
Ahmed Abas Al-Ali <bfhetyrhyr12@gmail.com>
2024-11-08 12:24:08 +0300

    update

--- src/components/RegisterContainer/RegisterContainer.jsx
+++ src/components/RegisterContainer/RegisterContainer.jsx
@@ -5,6 +5,10 @@ const RegisterContainer = ({children}) => {
             <div className="left">
                 <h1>Welcome to Your Freelancing Hub!</h1>
                 <p>We’re thrilled to have you here! Whether you're a client searching for skilled freelancers to bring your projects to life or a freelancer eager to find exciting opportunities, this is the place where connections are made, and ideas come to reality. Let's start building something amazing together!</p>
+                <div>
+                    <img src="./Vector4.png" alt="" />
+                    <img src="./Vector5.png" alt="" />
+                </div>
             </div>
             <div className="right">
                 {children}
